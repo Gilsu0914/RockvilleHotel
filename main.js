3e03f0f84b14de700fc213e2abef1844d739cc21
이길수 <bandicafe@igilsuui-MacBookPro.local>
2022-07-07 10:11:24 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const toggleBtn = document.querySelector(`.navbar_toggleBtn`);
+const menu = document.querySelector(`.navbar_menu`);
+
+toggleBtn.addEventListener(`click`, ()=>{
+    menu.classList.toggle(`active`);
+});
