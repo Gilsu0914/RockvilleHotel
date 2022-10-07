77f581e787ba5a8dfcd9359abc2892d7e54e31e4
GilSu Lee <109011151+Gilsu0914@users.noreply.github.com>
2022-10-07 13:07:36 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,83 +1,54 @@
 'use strict'
 
+
+//반응형 모바일 사이즈 메뉴 클릭
 const toggleBtn = document.querySelector(`.navbar_toggleBtn`);
 const menu = document.querySelector(`.navbar_menu`);
-
 toggleBtn.addEventListener(`click`, ()=>{
     menu.classList.toggle(`active`);
 });
 
 
-
-
-
-
-const deluxClass = document.querySelector(".delux");
-const suiteClass = document.querySelector(".suite");
-const familyClass = document.querySelector(".family");
-const presidentialClass = document.querySelector(".presidential");
-
-const hideImage = () => {
-    deluxClass.classList.remove('visible')
-    suiteClass.classList.remove('visible')
-    familyClass.classList.remove('visible')
-    presidentialClass.classList.remove('visible')
+//룸 종류별 버튼을 누르면 해당 사진과 설명만 보여주는 함수
+let roomBtn = document.querySelectorAll(`.roomBtn`);
+let roomContents = document.querySelectorAll(`.room_contents`);
+let hideImage =()=>{ 
+    for (let room_contents of roomContents ){
+        room_contents.classList.remove(`visible`)
+}}
+let makeWhite = ()=>{
+    for (let Btn of roomBtn){
+        Btn.style.backgroundColor = "white";
+    }
+}
+for(let i  = 0; i< roomBtn.length; i++){
+    roomBtn[i].addEventListener(`click`, ()=>{
+        hideImage();
+        makeWhite();
+        roomBtn[i].style.backgroundColor = "#da7f7c";
+        roomContents[i].classList.add(`visible`) 
+    })
 }
 
-const deluxBtn = document.getElementById("deluxBtn");
-const suiteBtn = document.getElementById("suiteBtn");
-const familyBtn = document.getElementById("familyBtn");
-const presidentialBtn = document.getElementById("presidentialBtn");
 
-deluxBtn.addEventListener('click', () => {
-    hideImage()
-    deluxClass.classList.add('visible')
-});
+//하단 버튼 캐로셀 동작
+let topSlideBtn = document.querySelectorAll(`.TopBtn`);
+for( let i = 0; i< topSlideBtn.length; i++ ){
+    topSlideBtn[i].addEventListener('click', ()=>{
+        if(topSlideBtn[i] == 0){
+            document.querySelector(`.container`).style.transform = `translateX(0vw)`;
+        }
+        else if(topSlideBtn[i] != 0){
+            let value = i * 33.3
+            document.querySelector(`.container`).style.transform = `translateX(-${value}%)`
+        }
+    })
+}
 
-suiteBtn.addEventListener('click', () => {
-    hideImage()
-    suiteClass.classList.add('visible')
-});
-
-familyBtn.addEventListener('click', () => {
-    hideImage()
-    familyClass.classList.add('visible')
-});
-
-presidentialBtn.addEventListener('click', () => {
-    hideImage()
-    presidentialClass.classList.add('visible')
-});
-
-
-
-document.querySelector(`.button2`).addEventListener(`click`,()=>{
-    document.querySelector(`.container`).style.transform = `translate(-33.3%)`;
-});
-
-document.querySelector(`.button3`).addEventListener(`click`,()=>{
-    document.querySelector(`.container`).style.transform = `translate(-66.6%)`;
-});
-
-document.querySelector(`.button1`).addEventListener(`click`,()=>{
-    document.querySelector(`.container`).style.transform = `translate(0vw)`;
-});
-
-
-
-
-document.querySelector(`.button22`).addEventListener(`click`,()=>{
-    document.querySelector(`.container2`).style.transform = `translate(-25%)`;
-});
-
-document.querySelector(`.button33`).addEventListener(`click`,()=>{
-    document.querySelector(`.container2`).style.transform = `translate(-50%)`;
-});
-
-document.querySelector(`.button44`).addEventListener(`click`,()=>{
-    document.querySelector(`.container2`).style.transform = `translate(-75%)`;
-});
-
-document.querySelector(`.button11`).addEventListener(`click`,()=>{
-    document.querySelector(`.container2`).style.transform = `translate(0%)`;
-});
+let bottomSlideBtn = document.querySelectorAll(`.DownBtn`);
+for(let i = 0; i<bottomSlideBtn.length; i++){
+    bottomSlideBtn[i].addEventListener(`click`, ()=>{
+        let value = i * 25
+        document.querySelector(`.container2`).style.transform = `translate(-${value}%)`;
+    })
+}
